337fa5d8b3e3ec367266a4390791742a54fe4675
JeremMac <macal.jerem@gmail.com>
2023-09-06 10:54:38 +0200

    Adding pop function.

--- monty.h
+++ monty.h
@@ -44,6 +44,7 @@ void free_stack(stack_t *stack);
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **head, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 
 #endif
---
+++ pop.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * pop - Removes the top element of the stack.
+ *
+ * @stack: a pointer to the top of the stack.
+ * @line_number: the line number being executed.
+ */
+
+void pop(stack_t **stack, unsigned int line_number)
+{
+	stack_t *current_node = NULL;
+
+	current_node = *stack;
+
+	if (stack == NULL)
+	{
+		fprintf(stderr, "L<%u>: can't pop an empty stack\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	current_node = current_node->next;
+	free(*stack);
+	*stack = current_node;
+}
