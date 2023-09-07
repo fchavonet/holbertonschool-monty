4d2fe8b4600e486b308a0d7e1b91190e49e78afa
JeremMac <macal.jerem@gmail.com>
2023-09-07 09:01:12 +0200

    Fix: fprintf forgotten

--- add.c
+++ add.c
@@ -12,7 +12,7 @@ void add(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%d: can't add, stack too short\n", line_number);
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
