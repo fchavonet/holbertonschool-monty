6928dd7b14b1d09cb605f1fc8f713ef140dc6048
JeremMac <macal.jerem@gmail.com>
2023-09-08 14:59:19 +0200

    Fix: betty indentation

--- pstr.c
+++ pstr.c
@@ -12,15 +12,15 @@ void pstr(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 
 	stack_t *current_node = *stack;
-    
-    while (current_node != NULL)
+
+	while (current_node != NULL)
 	{
-        if ((current_node->n < 65 ) || (current_node->n > 122))
-	    {
-            break;
-	    }
-        printf("%c", current_node->n);
+		if ((current_node->n < 65 ) || (current_node->n > 122))
+		{
+			break;
+		}
+		printf("%c", current_node->n);
 		current_node = current_node->next;
 	}
-    printf("\n");
+	printf("\n");
 }
